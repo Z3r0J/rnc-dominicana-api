cd4f6f1f3a4a8ac8fde2d207a3e94eb78cd89bb4
Z3r0J <tibboxcentral@gmail.com>
2023-02-26 18:29:29 -0400

    No-sandbox

--- CheckRNC.js
+++ CheckRNC.js
@@ -41,7 +41,9 @@ export const checkRNC = async (rnc) => {
 
     try {
         const browser = await puppeteer.launch({
-            headless: false, executablePath: chromium.path
+            headless: false,
+            executablePath: chromium.path,
+            args: ['--no-sandbox']
         });
         const page = await browser.newPage();
         await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36");
